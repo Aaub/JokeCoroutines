1f6a21974714a72d70b05258df1c0998267e5a51
alexandre_aubry <aaubry2@oui.sncf>
2018-05-03 09:37:35 +0200

    first commit

---
+++ app/src/main/java/com/example/alexandre_aubry/jokecouroutines/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.alexandre_aubry.jokecouroutines
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.experimental.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
